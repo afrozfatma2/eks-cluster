09019a4f2c8f3dc0f3078e31395df6fa03b10c98
Afroz <afroz00217@gmail.com>
2025-12-10 23:06:30 +0530

    eks

--- eks.tf
+++ eks.tf
@@ -1,13 +1,48 @@
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "demo-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0"
 
-  vpc_config {
-    subnet_ids              = data.aws_subnets.default.ids
-    security_group_ids      = [aws_security_group.eks_sg.id]
-    endpoint_private_access = true
-    endpoint_public_access  = false
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy_attach]
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2023_x86_64_STANDARD"
+    instance_types = ["t3.large"] 
+
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "eksnode-demo"
+      }
+    }
+  }
+
+  tags = local.tags
 }
